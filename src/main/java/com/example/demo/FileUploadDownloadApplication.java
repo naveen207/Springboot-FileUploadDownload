8875eb020464ff871ad7c7c1a6716105c2364c84
Singh <naveen.singh@bnc.ca>
2019-01-30 23:51:06 -0500

    second commit

---
+++ src/main/java/com/example/demo/FileUploadDownloadApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.demo.Fileproperty.FileStorageProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageProperties.class
+})
+public class FileUploadDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadDownloadApplication.class, args);
+	}
+
+}
+
